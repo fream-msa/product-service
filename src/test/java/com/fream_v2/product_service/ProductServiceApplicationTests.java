12e6d9570df89919674d4fe5803be1fca11af47a
jun <pickjog@gmail.com>
2025-11-16 21:19:43 +0900

    first commit

---
+++ src/test/java/com/fream_v2/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fream_v2.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
